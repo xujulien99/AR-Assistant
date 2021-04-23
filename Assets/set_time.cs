61bc385ddd27a7faa421657f9ae1bf8d5c54d9d2
xujulien99 <60070633+xujulien99@users.noreply.github.com>
2021-04-22 23:58:57 -0400

    commit

---
+++ Assets/set_time.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class set_time : MonoBehaviour
+{
+
+    public Text changingText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        update_time();
+    }
+
+    public void update_time()
+	{
+        string time = System.DateTime.UtcNow.ToString("t");
+        changingText.text = time;
+	}
+}
+
+
+/*
+ 
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class setTIme : MonoBehaviour
+{
+
+    public Text changingText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateTime();
+    }
+
+    void updateTime(){
+        changingText.text = System.DateTime.UtcNow.ToString("HH:mm:ss");
+    }
+}
+
+ */
